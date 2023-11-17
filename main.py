8725df90a3600e029f86ad7fc537bfcb7d79dab6
Mendaliz <ddv0707@mail.ru>
2023-11-17 22:06:33 +0300

    Sqlite btns add/change.

--- main.py
+++ main.py
@@ -1,30 +1,64 @@
 import sys
+import sqlite3
 
 from PyQt5 import uic
-from PyQt5.QtSql import QSqlDatabase, QSqlTableModel
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem
 
 
-class SqlCoffee(QWidget):
+class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        self.con = sqlite3.connect("coffee.sqlite")
+        self.pushButton.clicked.connect(self.update_result)
+        self.tableWidget.itemChanged.connect(self.item_changed)
+        self.pushButton_2.clicked.connect(self.save_results)
+        self.pushButton_3.clicked.connect(self.add_elem)
+        self.modified = {}
+        self.titles = None
 
-        db = QSqlDatabase.addDatabase('QSQLITE')
-        db.setDatabaseName('coffee.sqlite')
-        db.open()
+    def update_result(self):
+        cur = self.con.cursor()
+        result = cur.execute("SELECT * FROM Coffee WHERE " + self.textEdit.toPlainText()).fetchall()
+        self.tableWidget.setRowCount(len(result))
+        if not result:
+            self.statusBar().showMessage('Ничего не нашлось')
+            return
+        else:
+            self.statusBar().showMessage(f"Нашлась запись с id")
+        self.tableWidget.setColumnCount(len(result[0]))
+        self.titles = [description[0] for description in cur.description]
+        for i, elem in enumerate(result):
+            for j, val in enumerate(elem):
+                self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+        self.modified = {}
 
-        model = QSqlTableModel(self, db)
-        model.setTable('Coffee')
-        model.select()
+    def item_changed(self, item):
+        self.modified[self.titles[item.column()]] = item.text()
 
-        self.view.setModel(model)
+    def save_results(self):
+        if self.modified:
+            cur = self.con.cursor()
+            que = "UPDATE Coffee SET\n"
+            que += ", ".join([f"'{key}'='{self.modified.get(key)}'"
+                              for key in self.modified.keys()])
+            que += "WHERE id = ?"
+            cur.execute(que, (self.spinBox.text(),))
+            self.con.commit()
+            self.modified.clear()
+
+    def add_elem(self):
+        row = self.lineEdit.text().split(';')
+        st = f"INSERT INTO Coffee VALUES {tuple(row)}"
+        cur = self.con.cursor()
+        cur.execute(st)
+        self.con.commit()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = SqlCoffee()
+    ex = MyWidget()
     ex.show()
     sys.exit(app.exec())
-
 
